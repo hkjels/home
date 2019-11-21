5bbef2d3f6f64c40064168ad40e56c257875eb0b
Henrik Kjerringvåg <henrik@kjerringvag.no>
2019-11-21 21:43:45 +0100

    Improve documentation

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,4 +1,13 @@
+function sanetize(str) {
+  return str.toLowerCase().replace(/[^a-z]+/g, "_");
+}
+
 function DOMReady() {
+  let headings = [].slice.call(document.querySelectorAll("h2"));
+  headings.forEach((heading) => {
+    let className = sanetize(heading.innerText);
+    heading.classList.add(className);
+  });
 }
 
 addEventListener('DOMContentLoaded', DOMReady);
